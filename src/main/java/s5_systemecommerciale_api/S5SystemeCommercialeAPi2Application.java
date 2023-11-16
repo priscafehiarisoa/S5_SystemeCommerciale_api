610280d7c7ccb2039ad65c4a211b326d87f8729c
priscafehiarisoa <mytyrealy@gmail.com>
2023-11-16 07:50:58 +0300

    initial commit

---
+++ src/main/java/s5_systemecommerciale_api/S5SystemeCommercialeAPi2Application.java
@@ -0,0 +1,13 @@
+package s5_systemecommerciale_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5SystemeCommercialeAPi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S5SystemeCommercialeAPi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/s5_systemecommerciale_api/S5SystemeCommercialeAPi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package s5_systemecommerciale_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5SystemeCommercialeAPi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
